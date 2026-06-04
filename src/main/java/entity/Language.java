579cd2428c8d436ff7c26f42a2973391a062b5c8
Evgenii Vtorushin <evgeniivtorushin@yandex.ru>
2026-06-04 21:41:25 +0300

    project_hibernate_2

---
+++ src/main/java/entity/Language.java
@@ -0,0 +1,46 @@
+package entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "language")
+public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "language_id")
+    private Byte languageId;
+
+    @Column(columnDefinition = "char")
+    private String name;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+
+    public Byte getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Byte languageId) {
+        this.languageId = languageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+}
